469c45c7d7e1792b012f64328583b6802f0c3c81
adamahmed74 <adamahmed19600@gmail.com>
2023-12-18 09:14:58 +0200

    adam

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * Description: print a qoute using write function
- *  ssize_t write(int fd,const void *buf,size_t count)
+ * ssize_t write(int fd,const void *buf,size_t count)
  * Return: Always 1 (not success)
 */
 
