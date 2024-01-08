25f31147921b8a934a14bb39b6217b564c30d055
adamahmed74 <adamahmed19600@gmail.com>
2024-01-08 12:02:11 +0200

    aa

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -31,6 +31,13 @@ inc++;
 return (inc);
 }
 
+/**
+*swap_char - swap two characters
+*@a: first character
+*@b: second character
+*Return: nothing
+*/
+
 void swap_char(char *a, char *b)
 {
 char tmp = *a;
